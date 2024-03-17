f9632a7292fb1f4cdeda9aefa503d4482a2f1da2
Baba Hussain <bedadaluribabahussain2000@gmail.com>
2024-03-17 14:32:33 +0530

    c

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,7 +18,7 @@ function createTokenAndSendResponse(user, statusCode, res) {
   const cookieOptions = {
     httpOnly: true,
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      Date.now() + process.env.COOKIE_JWT_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
   };
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
